89e27de53d36cc94998e08ebca88e14be7a8cc8c
Peter Farmer <peter.m.farmer.204@gmail.com>
2023-10-22 17:22:01 +1100

    commands refactored out of main.go

--- main.go
+++ main.go
@@ -2,55 +2,8 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-type cliCommand struct {
-	name        string
-	description string
-	callback    func() error
-}
-
-func getCommands() map[string]cliCommand {
-	commands := map[string]cliCommand{
-		"help": {
-			name:        "help",
-			description: "Displays a help message",
-			callback:    commandHelp,
-		},
-		"exit": {
-			name:        "exit",
-			description: "Exit the pokedex",
-			callback:    commandExit,
-		},
-	}
-
-	return commands
-}
-
-func commandExit() error {
-	fmt.Println("Thank you for using the pokedex!")
-	fmt.Println("Goodbye")
-	os.Exit(0)
-	return nil
-}
-
-func commandHelp() error {
-	commands := getCommands()
-	fmt.Println("Welcome to the Pokedex!")
-	fmt.Println("-----------------------------")
-	fmt.Printf("%-5vUsage Guidelines\n", "")
-	fmt.Println()
-
-	for _, value := range commands {
-		fmt.Printf("%v: %v\n", value.name, value.description)
-		
-	}
-	fmt.Println()
-	fmt.Println("-----------------------------")
-
-	return nil
-}
 
 func main() {
 	var userInput string
